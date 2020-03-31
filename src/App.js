7129aea975475fe5e34d09d3a0869f6b309bf09d
takuya-unu <takuya0302u@gmail.com>
2020-04-01 04:43:52 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      name: ''
+    };
+  }
+  onInput = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+  
+  addTodo = () => {
+    const { todos, name } = this.state;
+    this.setState({
+      todos: [...todos, name]
+    });
+  }
+  removeTodo = (index) => {
+    const { todos, name } = this.state;
+    this.setState({
+      todos: [...todos.slice(0, index), ...todos.slice(index + 1)]
+    });
+  }
+  render() {
+    const { todos } = this.state;
+    return (<div>
+      <input type="text" onInput={this.onInput} />
+      <button onClick={this.addTodo} >登録</button>
+      <ul>
+        {todos.map((todo, index) => <li key={index}>
+          {todo}
+          <button onClick={() => { this.removeTodo(index) }}>削除</button>
+        </li>)}
+      </ul>
+    </div>);
+  }
 }
-
-export default App;
